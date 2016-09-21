971d6230a2b68ea06a82b01650c0640b19babfc7
Sam Alphonse <samalphonse@gmail.com>
2016-09-21 21:30:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-res.send('Article Two requested and will served here');
+res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-res.send('Article Three requested and will served here');
+res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {

ea73e908bc578776cf8b497337e9e84f11d23eda
Sam Alphonse <samalphonse@gmail.com>
2016-09-28 20:30:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,17 @@
 console.log('Loaded!');
+
 // change the text of the main-text div
 var element = document.getElementById('main-text');
+
 element.innerHTML='New Value';
+
+// move the image
 var face = document.getElementById('face');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    face.style.marginLeft = marginLeft + 'px';
+}
 face.onclick = function(){
-    face.style.marginLeft = '100px';
+   var interval = setInterval(moveRight,100);
 };

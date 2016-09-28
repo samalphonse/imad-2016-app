9acd1a8083eb5194dc0b5a09b4323468158ea70f
Sam Alphonse <samalphonse@gmail.com>
2016-09-28 19:59:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 // change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+var face = document.getElementById('face');
+face.onclick = function(){
+    face.style.marginLeft = '100px';
+};
